73e972b0922d50eb32172025f78b1d4f3b4e8012
Lucas Lima Cadiguni <lcadiguni@gvdasa.com.br>
2026-02-04 14:35:22 -0300

    refactor: remove unused webhookUrlTeams variable from inputs.tf

--- pipelines/terraform-function/inputs.tf
+++ pipelines/terraform-function/inputs.tf
@@ -5,12 +5,6 @@ variable "setor" {
   type = string
 }
 
-variable "webhookUrlTeams" {
-  type        = string
-  description = "URL do Webhook do Teams. Pode ser nulo ou vazio."
-  default     = null
-}
-
 variable "root_management_group" {
   description = "Management Group raiz onde estão todas as subscriptions"
   type        = string
